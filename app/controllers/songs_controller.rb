c10f41be314d68acbff00720e781ae3231185cf5
aemrich <adam.emrich@gmail.com>
2017-01-03 17:04:45 -0500

    done

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,49 @@
+class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params(:title, :artist_name, :release_year, :released, :genre))
+    if @song.save
+      redirect_to @song
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(song_params(:title, :artist_name, :release_year, :released, :genre))
+    if @song.save
+      redirect_to @song
+    else
+      render :edit
+    end
+
+    def destroy
+      @song = Song.find(params[:id])
+      @song.destroy
+      redirect_to songs_path
+    end
+  end
+
+  private
+
+  def song_params(*args)
+    params.require(:song).permit(*args)
+  end
+
+end
---
+++ app/models/song.rb
@@ -0,0 +1,24 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates :released, inclusion: {in: [true, false]}
+  validates :release_year, presence: true, if: :has_been_released?
+  validate :release_future
+  validates :artist_name, presence: true
+  validates_uniqueness_of :release_year, scope: [:artist_name, :title]
+
+  def release_future
+    if release_year
+      if release_year >= (Date.today.year + 1)
+        errors.add(:release_year, "in the future")
+      end
+    end
+  end
+
+  def has_been_released?
+    released == true
+  end
+
+  def song_in_same_year
+
+  end
+end
